f2496daf1ffd16f9aa910f0c51df4ff0af48c47a
mh_Earth <meherab0608@gmail.com>
2023-01-19 22:17:24 +0600

    fix navber

--- pages/9W1vw0PcCYNaXV6Pl3K4MgsjXQBsT2Gj/[pid].js
+++ pages/9W1vw0PcCYNaXV6Pl3K4MgsjXQBsT2Gj/[pid].js
@@ -54,7 +54,7 @@ const Email = (props) => {
             </Head>
 
 
-            <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+            <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
                 <div className="container flex flex-wrap items-center justify-between mx-auto">
                     <Link href="/9W1vw0PcCYNaXV6Pl3K4MgsjXQBsT2Gj" className="flex items-center">
                         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Email Hooker</span>
